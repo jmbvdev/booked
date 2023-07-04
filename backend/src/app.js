350d4e24e4bf6fdadcb6cd12f5425744a8fc813b
jome95 <jmbv.dev@gmail.com>
2023-07-04 23:53:14 +0200

    Set up server

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log("Listening to port ==> ", port);
+});
